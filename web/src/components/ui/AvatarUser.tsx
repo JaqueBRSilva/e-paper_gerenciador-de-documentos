e7fb6134755683cbefdc1a2d0ce55fb1d1164bea
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2024-11-19 22:09:50 -0300

    feat: criado componente 'AvatarUser'

---
+++ web/src/components/ui/AvatarUser.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils";
+import Image, { StaticImageData } from "next/image";
+import { Avatar } from "./avatar";
+
+interface AvatarUser_Props {
+    alt: string;
+    src: string | StaticImageData;
+    className?: string;
+}
+
+export default function AvatarUser({ ...imageProps }: AvatarUser_Props) {
+    return (
+        <Avatar>
+            <Image
+                alt={imageProps.alt}
+                src={imageProps.src}
+                className={cn(
+                    "aspect-square h-full w-full size-9",
+                    imageProps.className
+                )}
+            />
+        </Avatar>
+    )
+}
