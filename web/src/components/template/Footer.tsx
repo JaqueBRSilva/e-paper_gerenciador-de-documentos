c9594c10b8aa2ac4a7224d313966819d3242f797
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2024-11-20 15:40:55 -0300

    feat: criado template 'Footer'

---
+++ web/src/components/template/Footer.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import EPAPER_LOGO_CINZA from '../../../public/e-paper_cinza-svg.svg'
+
+export default function Footer() {
+    const actualYear = new Date().getFullYear()
+
+    return (
+        <footer
+            className="flex flex-1 absolute bottom-0 w-screen p-4 h-16 gap-3 justify-center place-items-center
+            bg-neutral border-t-2 border-gray500 border-opacity-5">
+
+            <Image
+                alt='e-paper logo'
+                src={EPAPER_LOGO_CINZA}
+                width={115}
+                priority
+            />
+
+            <p className='font-normal text-gray500 text-sm'>
+                Copyright © {actualYear} e-paper
+            </p>
+        </footer>
+    )
+}
