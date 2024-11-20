2987ca539e2d10ca717ca0d394be669598d38205
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2024-11-20 15:45:25 -0300

    feat: adicionado 'Footer' em 'Page'

--- web/src/components/template/Page.tsx
+++ web/src/components/template/Page.tsx
@@ -1,9 +1,12 @@
+import Footer from "./Footer";
 import Header from "./Header";
 
-export default function Page() {
+export default function Page(props: any) {
     return (
-        <div className="flex h-screen">
+        <div className="h-screen">
             <Header />
+            {props.children}
+            <Footer />
         </div>
     )
 }
