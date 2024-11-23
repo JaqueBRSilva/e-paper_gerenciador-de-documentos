55ecc5eca17ec02061b0b6bc4369bffc4fb0aad7
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2024-11-23 00:56:03 -0300

    feat: adicionado classificação das colunas em 'DataTable'

--- web/src/components/shared/DataTable/DataTable.tsx
+++ web/src/components/shared/DataTable/DataTable.tsx
@@ -5,8 +5,11 @@ import {
   Table, TableBody, TableCell, TableHead, TableHeader, TableRow,
 } from "@/components/ui/table"
 import {
-  ColumnDef, flexRender, getCoreRowModel, getPaginationRowModel, useReactTable,
+  ColumnDef, flexRender, getCoreRowModel, getPaginationRowModel, getSortedRowModel,
+  SortingState, useReactTable
 } from "@tanstack/react-table"
+import React from 'react'
+
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
@@ -17,11 +20,18 @@ export function DataTable<TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = React.useState<SortingState>([])
+
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    },
   })
 
   return (
